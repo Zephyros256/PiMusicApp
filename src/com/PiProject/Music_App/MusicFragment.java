69cdf4dab55e8323c2fb2bc6a16636117b65ca90
Robin van der Kruit <Robin.Kruit@hotmail.com>
2016-02-03 09:30:00 +0100

    Main.xml e.d. aagepast
    toevoegingen aan de manifest, main.xml, song.xml(niet zeker van hoe
    nuttig het was) strings.xml en de sonfragment aangepast

--- src/com/PiProject/Music_App/MusicFragment.java
+++ src/com/PiProject/Music_App/MusicFragment.java
@@ -1,7 +1,5 @@
 package com.PiProject.Music_App;
 
-/** TODO bestand eventueel hernoemen wanneer functie bepaald is **/
-
 import android.view.View;
 
 import android.app.Fragment;
@@ -17,7 +15,6 @@ public class MusicFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        /** TODO layout referentie updaten indien bestand etc. hernoemd is **/
         View rootView = inflater.inflate(R.layout.fragment_music, container, false);
 
         return rootView;
